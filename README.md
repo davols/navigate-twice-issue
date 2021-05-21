a7bc97e3576045dbcca1f8b69ec0e450b29e8149
David Olsson <david@davido.me>
2021-05-21 08:16:51 +0200

    Init commit with dummy project

---
+++ README.md
@@ -0,0 +1,3 @@
+While using androidx.navigation*-2.4.0-alpha01 user needs to navigate twice in order to navigate to dynamic feature module.
+
+This is just a dummy project to reproduce the issue. Start the app and notice you need to click the button twice. 
---
+++ app/src/main/java/com/example/navigationissue/BlankFragment.kt
@@ -0,0 +1,31 @@
+package com.example.navigationissue
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+
+
+class BlankFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+    var counter: Int = 0
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+                counter++
+            Toast.makeText(requireContext(), "Pressed $counter times", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(BlankFragmentDirections.toModule())
+        }
+    }
+
+}
